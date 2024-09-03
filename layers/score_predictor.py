90439bdfcb2deb12f9a983afdba03b0ca9d37dc0
张自恒 <63287643+chu-xuezhe@users.noreply.github.com>
2024-09-03 21:57:24 +0800

    Update code

--- layers/score_predictor.py
+++ layers/score_predictor.py
@@ -8,21 +8,20 @@ from torch.nn.modules.module import T
 class ScorePredictor(nn.Module):
     def __init__(self, in_features, hidden_edge_scores):
         super().__init__()
-        self.W1 = nn.Linear(3 * in_features, hidden_edge_scores)
-        self.W2 = nn.Linear(hidden_edge_scores, 1)
+        self.mlp1 = nn.Linear(3 * in_features, hidden_edge_scores)
+        self.mlp2 = nn.Linear(hidden_edge_scores, 1)
 
-    def apply_edges(self, edges):
+    def score_edges(self, edges):
         print("scoreing...")
         data = torch.cat([edges.src['x'], edges.dst['x'], edges.data['e']], dim=1)
-        h = self.W1(data)
+        h = self.mlp1(data)
         h = torch.relu(h)
-        score = self.W2(h)
+        score = self.mlp2(h)
         return {'score': score}
 
     def forward(self, graph, x, e):
-        print("score_predictor forward")
         with graph.local_scope():
             graph.ndata['x'] = x
             graph.edata['e'] = e
-            graph.apply_edges(self.apply_edges)
+            graph.apply_edges(self.score_edges)
             return graph.edata['score']
